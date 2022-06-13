0e62848665773b4717ff3309f10417c2e6735a44
Natalie Lima <nat.lima86@gmail.com>
2022-06-12 22:21:20 -0300

    Montando o combo dinamicamente

--- src/main/java/br/com/spring/controller/TituloController.java
+++ src/main/java/br/com/spring/controller/TituloController.java
@@ -1,11 +1,16 @@
 package br.com.spring.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.spring.model.StatusTitulo;
 import br.com.spring.model.Titulo;
 import br.com.spring.repository.TitulosRepository;
 
@@ -17,8 +22,10 @@ public class TituloController {
 	private TitulosRepository titulosRepository;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -31,6 +38,11 @@ public class TituloController {
 		
 	}
 	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+		
+	}
 		
 	
 
