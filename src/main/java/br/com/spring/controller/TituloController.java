cfdc6aa8582cf325cf548fba17b67a3ee4cbea27
Natalie Lima <nat.lima86@gmail.com>
2022-06-13 00:00:14 -0300

    Protótipo da pesquisa de títulos

--- src/main/java/br/com/spring/controller/TituloController.java
+++ src/main/java/br/com/spring/controller/TituloController.java
@@ -37,6 +37,12 @@ public class TituloController {
 		return mv;
 		
 	}
+	@RequestMapping
+	public String pesquisar() {
+		
+		return "PesquisaTitulos";
+	}
+	
 	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo(){
